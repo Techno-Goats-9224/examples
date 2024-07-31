8c4dab00dead842bbd1fc2addd8d775a519334c7
Techno-Goats-9224 <90360768+Techno-Goats-9224@users.noreply.github.com>
2024-07-30 22:39:27 -0500

    Update time-based.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/time-based.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/time-based.java
@@ -1,1 +1,27 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class blankLinearOpMode extends LinearOpMode {
+
+  //make an object of the time, you can change runtime to whatever name you want
+  private ElapsedTime runtime = new ElapsedTime();
+    
+    @Override
+    public void runOpMode() {
+      
+        // Wait for driver to press start -- don't remove this line
+        waitForStart();
+
+        //reset your runtime before each action
+        runtime.reset();
+        //this is the easiest way to control movements on the robot in auto
+        //this can be combined with sensors or some other check as a fail-safe
+        while(runtime.seconds() < /*pick your number*/ && opModeIsActive()){
+          //do thing
+        }
+      
+    } //close runOpMode()
+} // close entire class
