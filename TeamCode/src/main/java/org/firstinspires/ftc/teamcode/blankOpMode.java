ca9d1ad695ed98860a4f39927653ff6cc00b5d82
Techno-Goats-9224 <90360768+Techno-Goats-9224@users.noreply.github.com>
2024-07-30 22:25:30 -0500

    Create blankOpMode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blankOpMode.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//Copy this as the base for most normal programs (the other option is blankLinearOpMode.java
+/**
+From FTC
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+//this annotation makes is show up on the right drop-downn arrow on the driver station
+@TeleOp
+//add this annotation if you want the code to show up on the computer but not on the driver station
+//@Disabled
+//this class extends OpMode, which gives it all the basic FTC strcture
+public class blankOpMode extends OpMode
+{
+    //declare motor, servo, sensor variables here
+  
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        //put hardwareMap.get, sensor, and servo initialization here
+      
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+      //for safety, set all motor powers to 0 here
+    }
+
+}
